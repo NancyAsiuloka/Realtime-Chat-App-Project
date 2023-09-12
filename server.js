2f217c9e7b7fbe1354210a12029c92aa9695d4f8
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-12 09:26:49 +0100

    added some changes

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser} = require('./utils/users');
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+} = require('./utils/users');
 
 
 const app = express();
